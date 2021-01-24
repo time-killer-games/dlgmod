0fb12ec5251de8e27b87eb760e8f324b6f7d2ce5
time-killer-games <samuelvenable@hotmail.com>
2021-01-24 18:54:34 -0500

    Update dlgmod (x64).sh

--- dlgmod (x64).sh
+++ dlgmod (x64).sh
@@ -1,6 +1,6 @@
 cd "${0%/*}"
 clang++ -c -std=c++17 "dlgmod.part1.cpp" "main.cpp" "config.cpp" -fPIC -m64
-clang++ -c -ObjC "dlgmod.part2.mm" "subclass.mm" -fPIC -m64
+clang++ -c -ObjC "dlgmod.part2.m" "subclass.m" -fPIC -m64
 clang++ "dlgmod.part1.o" "dlgmod.part2.o" "subclass.o" "config.o" "main.o" -o "dlgmod (x64)/dlgmod" -framework Cocoa -fPIC -m64
 rm -f "dlgmod.part1.o" "dlgmod.part2.o" "subclass.o" "config.o" "main.o"
 chmod 777 "dlgmod (x64)/dlgmod"
