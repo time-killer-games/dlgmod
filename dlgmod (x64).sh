d91cddc05b99ff7a8f2ef745d3575a9e95f3229d
time-killer-games <samuelvenable@hotmail.com>
2021-03-11 20:25:28 -0500

    Add files via upload

--- dlgmod (x64).sh
+++ dlgmod (x64).sh
@@ -1,4 +1,4 @@
 cd "${0%/*}"
 
 mkdir "DlgModule (x64)"
-clang++ "dlgmodule.mm" "subclass.mm" "setpolicy.mm" "main.cpp" "config.cpp" -o "DlgModule (x64)/dlgmod" -std=c++17 -ObjC++ -framework Cocoa -fPIC -m64
+clang++ "dlgmodule.mm" "setpolicy.mm" "main.cpp" "config.cpp" -o "DlgModule (x64)/dlgmod" -std=c++17 -ObjC++ -framework Cocoa -fPIC -m64
